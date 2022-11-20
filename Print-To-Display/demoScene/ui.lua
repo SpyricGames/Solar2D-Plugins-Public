d5a948c9826d9612ce51b9fa71c9efd04bff1fed
XeduR <eetu.rantanen@spyric.com>
2022-11-20 07:30:07 +0200

    Print To Display: Add new features to demoScene
    Add better support for orientation and size changes to the demoScene and add a resize event to utilise the new resize function in the Print to Display plugin.

--- Print-To-Display/demoScene/ui.lua
+++ Print-To-Display/demoScene/ui.lua
@@ -6,45 +6,72 @@
 --  /____/ .___/\__, /_/  /_/\___/   \____/\__,_/_/ /_/ /_/\___/____/    --
 --      /_/    /____/                                                    --
 --                                                                       --
---  © 2020-2021 Spyric Games Ltd.             Last Updated: 21 May 2021  --
+--  © 2020-2022 Spyric Games Ltd.         Last Updated: 20 November 2022 --
 ---------------------------------------------------------------------------
 --  License: MIT                                                         --
 ---------------------------------------------------------------------------
 
 local ui = {}
 
+local spyricLogo, banner, header, footer, description
+
 local headerHeight = 80
 local footerHeight = 80
+local minPadding = 20
+local logoPadding = 4
+local colorText = { 252/255, 186/255, 4/255 }
+local colorBG = { 20/255, 21/255, 24/255 }
+local colorBanner = { 8/255 }
 
--- Create convenience properties for positioning the sample project contents.
+ui.headerHeight = headerHeight
+ui.footerHeight = footerHeight
+
 local function onResize( event )
-	display.minY = display.screenOriginY + headerHeight
-	display.maxY = display.contentHeight - display.screenOriginY - footerHeight
+	-- Create convenience properties for positioning the sample project contents.
+	ui.minY = display.screenOriginY + headerHeight
+	ui.maxY = display.contentHeight - display.screenOriginY - footerHeight
+
+	-- Update header elements:
+	banner.x, banner.y = display.contentCenterX, display.screenOriginY + headerHeight*0.5
+	header.x, header.y = banner.x, banner.y
+	spyricLogo.x = display.contentWidth - display.screenOriginX - spyricLogo.width*0.5 - 4
+	spyricLogo.y = banner.y
+	banner.width = display.actualContentWidth
+
+	local spaceTop = banner.width - (logoPadding + spyricLogo.width + minPadding)*2
+	local scaleTop = math.min( 1, spaceTop / header.width )
+	header.xScale, header.yScale = scaleTop, scaleTop
+
+	-- Update footer elements:
+	footer.x, footer.y = display.contentCenterX, display.contentHeight - display.screenOriginY - footerHeight*0.5
+	footer.width = display.actualContentWidth
+	description.x, description.y = footer.x, footer.y
+
+	local spaceBottom = footer.width - minPadding*2
+	local scaleBottom = math.min( 1, spaceBottom / description.width )
+	description.xScale, description.yScale = scaleBottom, scaleBottom
 end
 
 -- Create the default UI that is shared by all Spyric Games sample project.
-function ui.create( projectName, isFree )
-	local projectName = projectName or "Untitled Project"
-	local isFree = isFree and true or false
-		
+function ui.create( projectName )
 	display.setStatusBar( display.HiddenStatusBar )
-	display.setDefault( "background", 20/255, 21/255, 24/255 )
-	
-	local banner = display.newRect( display.contentCenterX, display.screenOriginY + headerHeight*0.5, display.actualContentWidth, headerHeight )
-	banner:setFillColor( 0.03 )
-	
-	local spyricLogo = display.newImageRect( "demoScene/spyric-logo.png", 64, 64 )
-	spyricLogo.x = display.contentWidth - display.screenOriginX - spyricLogo.width*0.5 - 4
+	display.setDefault( "background", unpack(colorBG) )
+
+	banner = display.newRect( display.contentCenterX, display.screenOriginY + headerHeight*0.5, display.actualContentWidth, headerHeight )
+	banner:setFillColor( unpack(colorBanner) )
+
+	spyricLogo = display.newImageRect( "demoScene/spyric-logo.png", 64, 64 )
+	spyricLogo.x = display.contentWidth - display.screenOriginX - spyricLogo.width*0.5 - logoPadding
 	spyricLogo.y = banner.y
 
-	local header = display.newText( projectName .. " - Sample Project", banner.x, banner.y, "demoScene/font/Roboto-Black.ttf", 32 )
-	header:setFillColor( 252/255, 186/255, 4/255 )
-	
-	local footer = display.newRect( display.contentCenterX, display.contentHeight - display.screenOriginY - footerHeight*0.5, display.actualContentWidth, footerHeight )
-	footer:setFillColor( 0.03 )
+	header = display.newText( projectName .. " - Sample Project", banner.x, banner.y, "demoScene/font/Roboto-Black.ttf", 32 )
+	header:setFillColor( colorText[1], colorText[2], colorText[3] )
 
-	local description = display.newText({
-	    text = projectName .. " is a " .. (isFree and "free" or "premium") .. " plugin for Solar2D.\nFor documentation, please visit https://docs.spyric.com/.",
+	footer = display.newRect( display.contentCenterX, display.contentHeight - display.screenOriginY - footerHeight*0.5, display.actualContentWidth, footerHeight )
+	footer:setFillColor( unpack(colorBanner) )
+
+	description = display.newText({
+	    text = projectName .. " is a free plugin for Solar2D.\nFor documentation, please visit https://docs.spyric.com/.",
 	    x = footer.x,
 	    y = footer.y,
 	    width = 560,
@@ -52,17 +79,15 @@ function ui.create( projectName, isFree )
 	    fontSize = 20,
 	    align = "center"
 	})
-	description:setFillColor( 252/255, 186/255, 4/255 )
-	
-	if system.getInfo( "environment" ) == "simulator" then
-		description:addEventListener( "touch", function(event)
-			if event.phase == "ended" then
-				system.openURL( "https://docs.spyric.com" )
-			end
-			return true
-		end )
-	end
-	
+	description:setFillColor( unpack(colorText) )
+
+	description:addEventListener( "touch", function(event)
+		if event.phase == "ended" then
+			system.openURL( "https://docs.spyric.com" )
+		end
+		return true
+	end )
+
 	Runtime:addEventListener( "resize", onResize )
 	onResize()
 end
--- Print-To-Display/main.lua
+++ Print-To-Display/main.lua
@@ -6,31 +6,35 @@
 --  /____/ .___/\__, /_/  /_/\___/   \____/\__,_/_/ /_/ /_/\___/____/    --
 --      /_/    /____/                                                    --
 --                                                                       --
---  © 2020-2021 Spyric Games Ltd.            Last Updated: 20 June 2021  --
+--  © 2020-2022 Spyric Games Ltd.         Last Updated: 20 November 2022 --
 ---------------------------------------------------------------------------
 --  License: MIT                                                         --
 ---------------------------------------------------------------------------
 
 -- Set up the demo scene UI.
-local demoScene = require( "demoScene.ui" ).create( "Spyric Print To Display", true )
+local ui = require( "demoScene.ui" )
+ui.create( "Spyric Print To Display" )
 
 -----------------------------------------------------------------------
 
 -- Require Spyric Print to Display.
 local printToDisplay = require( "spyric.printToDisplay" )
+local consoleWidth = 300
 
 -- Start the in-app console with custom visual settings.
 printToDisplay.start({
-    height = display.maxY - display.minY,
-	y = display.minY,
-    alpha=0.9,
+    width = consoleWidth,
+    height = ui.maxY - ui.minY,
+    x = display.screenOriginX,
+	y = ui.minY,
+    alpha = 0.9,
 
-    -- Create a single custom button, just to show how it's done.
+    -- Create a single custom button, just to show how it can be done.
     customButton = {
         {
             listener = function()
-                -- You could run some ad, analytics, debugging tests here, etc.
-                print( "You pressed the custom \"star\"-button!\nYou can add all types of custom buttons with their custom functionality to the in-app console."  )
+                -- You could run some ad, analytics, debugging tests here, print the contents of some table, etc.
+                print( "Running a custom function." )
             end,
             fontSize = 24,
             id = "⭐"
@@ -38,33 +42,65 @@ printToDisplay.start({
     }
 })
 
+
 -----------------------------------------------------------------------
 
--- You can write print() commands here and they'll appear in the in-app console.
+
+-- After Print To Display has been started, whenever you call the normal print() function,
+-- its output will be displayed both in the Solar2D console and in the in-app console.
 print("This is the in-app console.")
 print("")
+print("Using this plugin makes wireless on-device debugging quick and easy.")
+print("")
 
--- Add simple one sentence explanations for what the in-app console buttons do.
-local labelX = display.screenOriginX+display.actualContentWidth/3+40
-local toggleText = display.newText( "❮  Hide/show the console", labelX, display.screenOriginY+95, "demoScene/font/Roboto-Regular.ttf", 20 )
+
+-----------------------------------------------------------------------
+
+-- Add simple instructions.
+local textX = display.screenOriginX + consoleWidth + 40
+local toggleText = display.newText( "❮  Hide/show the console", textX, ui.minY+15, "demoScene/font/Roboto-Regular.ttf", 20 )
 toggleText.anchorX = 0
-local resumeText = display.newText( "❮  Pause/Resume autoscroll", labelX, display.screenOriginY+137, "demoScene/font/Roboto-Regular.ttf", 20 )
+local resumeText = display.newText( "❮  Pause/Resume autoscroll", textX, ui.minY+57, "demoScene/font/Roboto-Regular.ttf", 20 )
 resumeText.anchorX = 0
-local clearText = display.newText( "❮  Clear all outputs", labelX, display.screenOriginY+179, "demoScene/font/Roboto-Regular.ttf", 20 )
+local clearText = display.newText( "❮  Clear all outputs", textX, ui.minY+99, "demoScene/font/Roboto-Regular.ttf", 20 )
 clearText.anchorX = 0
-local userFunction = display.newText( "❮  Run a custom user-defined function.", labelX, display.screenOriginY+223, "demoScene/font/Roboto-Regular.ttf", 20 )
+local userFunction = display.newText( "❮  Run a custom function.", textX, ui.minY+143, "demoScene/font/Roboto-Regular.ttf", 20 )
 userFunction.anchorX = 0
 
-local description = display.newText(
-    "With Spyric Print To Display plugin, whenever you use the print() function, " ..
-    "the output will be sent to the simulator console and to an in-app console.\n\n" ..
-    "Having an in-app console allows you to easily debug your apps on your devices during testing.",
-    500, 280, 440, 0, "demoScene/font/Roboto-Regular.ttf", 20
-)
+local description = display.newText({
+    text = "Whenever you use print(), the output is displayed both in the Solar2D console and in the in-app console to the left.",
+    x = textX - 20,
+    y = ui.minY+200,
+    width = 260,
+    font = "demoScene/font/Roboto-Regular.ttf",
+    fontSize = 20,
+})
 description.anchorX, description.anchorY = 0, 0
 
--- Lazy, yet effective button creation.
-local button = require("button")
-for i = 1, 2 do
-    local btn = button.new( i )
-end
+
+-----------------------------------------------------------------------
+
+Runtime:addEventListener( "resize", function()
+
+    -------------------------------------------------------------------
+    -- The resize function can update the console's height and x/y position,
+    -- which is useful if you wish to support multiple orientations.
+
+    printToDisplay.resize({
+        -- This height is equal to display.actualContentHeight, minus the demoScene banners.
+        height = ui.maxY - ui.minY,
+        x = display.screenOriginX,
+        -- This y is equal to display.screenOriginY, plus the top banner's height.
+        y = ui.minY,
+    })
+    -------------------------------------------------------------------
+
+    -- Update the text positions.
+    local textX = display.screenOriginX + consoleWidth + 40
+    toggleText.x, toggleText.y = textX, ui.minY+15
+    resumeText.x, resumeText.y = textX, ui.minY+57
+    clearText.x, clearText.y = textX, ui.minY+99
+    userFunction.x, userFunction.y = textX, ui.minY+143
+
+    description.x, description.y = textX - 20, ui.minY+200
+end )
